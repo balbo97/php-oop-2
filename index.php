b8c602b5bbfc6c31596bb7175136ef35b744de7d
balbo97 <balbosauro1@gmail.com>
2024-02-01 15:30:02 +1000

    creazio della classe CATEGORIA

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-    
+
+// <!-- definisco la classe CATEGORIA  -->
+class Categoria {
+    public $name 
+
+    public function __construct($name) {
+        $this->name = $name;
+    }
+}
+
 ?>
