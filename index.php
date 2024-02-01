0a72f9afa997da672ef337f5a61b3f107fd3b59a
balbo97 <balbosauro1@gmail.com>
2024-02-01 16:07:50 +1000

    creazione della classe PRODOTTO

--- index.php
+++ index.php
@@ -9,4 +9,28 @@ class Categoria {
     }
 }
 
+class Prodotto {
+    public $name;
+    public $prezzo;
+    public $categoria;
+    public $immagine;
+
+    public function __construct($name, $prezzo, Categoria $categoria, $immagine, $tipo) {
+        $this->name = $name;
+        $this->prezzo = $prezzo;
+        $this->categoria = $categoria;
+        $this->immagine = $immagine;
+    }
+
+    public function stampaDettagli() {
+        echo "<div>";
+        echo "<img src='{$this->immagine}' alt='{$this->nome}'>";
+        echo "<h3>{$this->nome}</h3>";
+        echo "<p>Prezzo: €{$this->prezzo}</p>";
+        echo "<p>Categoria: {$this->categoria->name}</p>";
+        echo "</div>";
+    }
+}
+
 ?>
+
